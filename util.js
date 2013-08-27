dbbd42278665e92b7ab442f7bacbc2b81837018f
Ionica Bizau <bizauionica@yahoo.com>
2013-08-27 09:58:35 +0300

    add createUrl function in util

--- util.js
+++ util.js
@@ -1,3 +1,19 @@
 var Util = require("util");
+var API_URL = "https://www.googleapis.com/youtube/v3/";
 
-// TODO
+function createUrl (api, options) {
+
+    var url = API_URL + api;
+
+    options.access_token = (this.Client.getConfig().auth || {}).token;
+
+    var i = -1;
+    for (var opt in options) {
+        var value = options[opt];
+        url += (++i === 0 ? "?" : "&") + opt + "=" + value;
+    }
+
+    return url;
+}
+
+exports.createUrl = createUrl;
