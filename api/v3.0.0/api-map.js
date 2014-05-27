8c2e858f031a13ba31357ca314faaef5bc43ee0a
Ionică Bizău <bizauionica@yahoo.com>
2014-05-27 11:39:29 +0300

    Initial version of added API map

---
+++ api/v3.0.0/api-map.js
@@ -0,0 +1,103 @@
+// Constants
+const API_URL = "https://www.googleapis.com/youtube/v3/";
+
+// Methods
+const METHODS = {
+    get: {
+        options: {
+            method: "GET"
+        }
+    }
+  , post: {
+        options: {
+            method: "POST"
+        }
+    }
+  , update: {
+        options: {
+            method: "PUT"
+        }
+    }
+  , delete: {
+        options: {
+            method: "DELETE"
+        }
+    }
+};
+
+// The YouTube API map
+var ApiMap = {
+    "activities": {
+        "methods": {
+            "list": METHODS.get
+          , "insert": METHODS.post
+        }
+    }
+  , "channelBanners": {
+        "methods": {
+            "insert": METHODS.post
+        }
+    }
+  , "channels": {
+        "methods": {
+            "list": METHODS.get
+          , "update": METHODS.put
+        }
+
+    }
+  , "guideCategories": {
+        "methods": {
+            "list": METHODS.get
+        }
+
+    }
+  , "playlistItems": {
+        "methods": {
+            "list": METHODS.get
+          , "insert": METHODS.post
+          , "update": METHODS.put
+          , "delete": METHODS.delete
+        }
+
+    }
+  ,"playlists": {
+        "methods": {
+            "list": METHODS.get
+          , "insert": METHODS.post
+          , "update": METHODS.put
+          , "delete": METHODS.delete
+        }
+    }
+  ,"search": {
+        "methods": {
+            "list": METHODS.get
+        }
+    }
+  ,"subscriptions": {
+        "methods": {
+            "list": METHODS.get
+          , "insert": METHODS.post
+          , "delete": METHODS.delete
+        }
+    }
+  ,"thumbnails": {
+        "methods": {
+            "set": METHODS.put
+        }
+    }
+  ,"videoCategories": {
+        "methods": {
+            "list": METHODS.get
+        }
+    }
+  ,"videos": {
+        "methods": {
+            "getRating": METHODS.get
+          , "rate": METHODS.post
+          , "list": METHODS.get
+          , "insert": METHODS.post
+          , "update": METHODS.put
+          , "delete": METHODS.delete
+        }
+    }
+};
